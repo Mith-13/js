0e2a57b07091b18c0e48bd0ed9a5721ce402b014
Mith-13 <165086339+Mith-13@users.noreply.github.com>
2024-04-17 10:36:54 +0000

    add a file

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+ console.log("2">1);
+ console.log("02">1);
+
+console.log(null>0);   
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//===("strict check it means it also check the data type")
+
+console.log("2"===2);
+console.log("2"==2);
