4c4d203e3dbf87ae230427bae57cde63ad8bb2aa
Mith-13 <165086339+Mith-13@users.noreply.github.com>
2024-04-17 10:10:18 +0000

    add a file

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -2,24 +2,24 @@ let score = "33abc"
 let mask = null
 let score1 = undefined
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 let valueInNum=Number(mask)
 let ValueInscore1=Number(score1)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);//it gives a NaN as output means it is not a number
-console.log(mask);
-console.log(typeof(mask))
-console.log(valueInNum);
-console.log(score1);
-console.log(ValueInscore1);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);//it gives a NaN as output means it is not a number
+// console.log(mask);
+// console.log(typeof(mask))
+// console.log(valueInNum);
+// console.log(score1);
+// console.log(ValueInscore1);
 //"33" =>33
 //"33abc" =>NaN
 //true => 1; false =>0
 let isLoggedIn = "mithilesh";
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1=> true 0 =>false
 //"" =>false
 // "mithilesh" =>true
@@ -27,5 +27,46 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+// ******************************operation*********************************
+
+
+let value = 3
+let negValue = -value
+// //console.log(negValue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+let str1="hello"
+let str2="mithilesh"
+let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+// console.log(3+4*5%3);
+//  console.log(+true);
+ // true=> true
+
+//  +true is possible and output is 1
+// but true+ is not possible it gives error msg
+/* in type conversion if first data is number then operation based on number and if it is string 
+then operation based on string */
+// console.log(+"");
+let num1,num2,num3
+
+num1=num2=num3=2+2
+let gameCounter = 100
+let m=23
+gameCounter++;
+m++
+console.log(m);
+console.log(gameCounter);
+console.log(m++);
+console.log(++m)
